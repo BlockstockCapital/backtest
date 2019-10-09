bb2a04be0670ccb59e4e5a244cf9becc6684660b
ishest <io.shestopalov@gmail.com>
2019-10-09 21:02:31 +0300

    test!

--- MA_backtest.py
+++ MA_backtest.py
@@ -5,8 +5,3 @@ from event import SignalEvent, EventType
 from strategy import AbstractStrategy
 
 
-test = 'lalalala'
-test2 = 'lalalal'
-test3 = 'unresolved issue'
-
-motto = "Let's earn money dude!!!"
